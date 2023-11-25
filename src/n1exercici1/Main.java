352bbb0003f1a0f9b4074e0a5b4ace40b0d44eb7
Osbiju <okeshik@protonmail.com>
2023-11-25 16:43:39 +0100

    ok, repassar per entregar, falta pulir

--- src/n1exercici1/Main.java
+++ src/n1exercici1/Main.java
@@ -1,2 +1,67 @@
-package n1exercici1;public class Main {
+package n1exercici1;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Month> months = new ArrayList<>();
+
+        Month month1 = new Month("gener");
+        Month month2 = new Month("febrer");
+        Month month3 = new Month("març");
+        Month month4 = new Month("Abril");
+        Month month5 = new Month("Maig");
+        Month month6 = new Month("juny");
+        Month month7 = new Month("Juliol");
+        Month month8 = new Month("Agost");
+        Month month9 = new Month("Septembre");
+        Month month10 = new Month("octubre");
+        Month month11 = new Month("novembre");
+        Month month12 = new Month("desembre");
+
+        months.add(month1);
+        months.add(month2);
+        months.add(month3);
+        months.add(month4);
+        months.add(month5);
+        months.add(month6);
+        months.add(month7);
+        //aniria agost
+        months.add(month9);
+        months.add(month10);
+        months.add(month11);
+        months.add(month12);
+        System.out.println("El mesos sense Agost:\n" + months.toString());
+
+        months.add(7, month8); //afegeixo agost despres i estara al seu lloc
+
+        //probo afegir un mes repetit
+        HashSet<Month> hashMonth = new HashSet<Month>(months);
+
+        System.out.println("Aquiiiii");
+        hashMonth.add(month9);
+
+
+
+        System.out.println("Els mesos amb Agost:\n" + months.toString());
+
+        System.out.println("Els mesos remenats:\n" + hashMonth);
+
+        System.out.println("Recorro la llista amb for i la imprimeixo:");
+        for (Month month : months) {
+            System.out.println(month.getNom());
+        }
+
+        //recorro la llista amb iterador
+        System.out.println("hashSet months:");
+        Iterator<Month> iterator = hashMonth.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+
+    }
 }
--- src/n1exercici1/Month.java
+++ src/n1exercici1/Month.java
@@ -1,2 +1,21 @@
-package n1exercici1;public class Month {
+package n1exercici1;
+
+public class Month {
+    private String nom;
+
+    public Month(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String toString() {
+        return this.getNom();
+    }
 }
